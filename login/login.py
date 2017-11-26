bbefed3bdf211dad4f5361796eb06ae059aa30aa
magicdu <johndo@magicdu.cn>
2017-11-26 10:08:35 +0800

    add login

--- login/login.py
+++ login/login.py
@@ -28,9 +28,11 @@ def setHeader(url):
 	index=random.randint(0,length-1)
 	user_agent = user_agents[index]
 	headers={
+		'authority':host,
 		'Referer': url,
-		'Host':host,
 		'User-Agent':user_agent,
+		'scheme':'https',
+		'cookie':'''PB3_SESSION="2|1:0|10:1511533780|11:PB3_SESSION|40:djJleDoxNzEuMjEwLjIxMy42Njo5MTIwOTM0OQ==|e009e48c1123d62634b84b4042b1738071babb5b958a2bfad37e1cd3df6aaf90"; V2EX_TAB="2|1:0|10:1511535264|8:V2EX_TAB|8:dGVjaA==|dc85fae96ecd358395251f7b8dd4b262b8c7539186567c07d64bbd5c506dbaa1"; _gat=1; V2EX_LANG=zhcn; _ga=GA1.2.768685691.1494208860; _gid=GA1.2.522141493.1511533782''',
 		'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
 	}
 	return headers
@@ -42,8 +44,10 @@ def getParams(url):
 	print(paramDoc.status_code)
 	bsObj=BeautifulSoup(paramDoc.content,'html.parser')
 	for inmark in bsObj.find_all('input'):
-		name=inmark.attrs['name']
-		print(name)
+		#print(inmark)
+		if('name' in inmark.attrs):
+			name=inmark.attrs['name']
+			print(name)
 
 
 
