57f2c7c3b212d2654b7a211b9d79482276573c44
duziqiang <2760522291@qq.com>
2019-10-15 17:36:05 +0800

    add django

--- redis/spider_redis.py
+++ redis/spider_redis.py
@@ -3,8 +3,10 @@
 import redis
 ip = "localhost"
 port = 6379
-password =
-
+password = ""
+key = "hello"
+value = "world"
 r = redis.Redis(host=ip, port=port, password=password, decode_responses=True)
 r.set(key, value)
-r.get(key)
+a = r.get(key)
+print(a)
