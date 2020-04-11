73cca057051947fbfbca2cf54d0beae7296c6353
cberranger <chris.berranger@gmail.com>
2020-04-12 01:09:38 +0400

    Update getArticleInCSV.py

--- getArticle/getArticleInCSV.py
+++ getArticle/getArticleInCSV.py
@@ -16,21 +16,20 @@ def get_article_title(url):
 url=('http://www.jrzj.com/global/')
 with open('articels.csv','w') as f:
 	writer=csv.writer(f)
-	writer.writerow(('标题','标签','发布时间'))
+	writer.writerow(('title','tag','posttime'))
 #while url!='http://www.jrzj.com/global/list-9-1483-0.html':
 
-#i为爬取多少页，每页10条
+# i is how many pages to crawl, 10 is how many per page
 for i in range(10):
-	print('当前爬取的页面：-------->%s'%url)
+	print('Currently Crawled Pages：-------->%s'%url)
 	res=requests.get(url)
 	res.raise_for_status()
 	soup=bs4.BeautifulSoup(res.text,'html.parser')
-	#找到div --'zuixinfabu'--->找到所有dd标签
+	# Find div-'zuixinfabu' ---> find all dd tags
 	dd_list=soup.find('div',{'class':'zuixinfabu'}).findAll('dd')
-	#遍历所有dd标签，取得每篇文章的链接
-	for dd in dd_list:
+	# Traverse all dd tags to get a link to each article	for dd in dd_list:
 		articleUrl=dd.find('a',{'target':'_blank'}).get('href')
-		print('正在爬取:----->%s'%articleUrl)
+		print('Crawling:----->%s'%articleUrl)
 		get_article_title(articleUrl)
 
 	prevLinks=soup.find('div',{'class':'fy_big'}).findAll('a',{'class':'a1'})
